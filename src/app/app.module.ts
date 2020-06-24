bf02ba6a2a470d9dada025d5cbf4bcd642ab5bd9
nurdared <nurda9727@gmail.com>
2020-06-24 17:52:28 +0600

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer })
+  ],
+  declarations: [ AppComponent, MyCounterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
